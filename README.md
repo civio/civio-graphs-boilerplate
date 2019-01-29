20c18195968dddcd644efddefca6a25e3a72fdad
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-01-29 18:14:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Civio Graphs Boilerplate
+
+Civio graphs boilerplate with Webpack 4, Babel 7, Sass, Linting & Hot Reload
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist')
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    port: 5000,
+    stats: 'minimal'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+          
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    })
+  ]
+};
