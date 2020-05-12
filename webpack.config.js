4a6409574f93ff16827adaeb71d69f123afddf1b
David Cabo <david.cabo@gmail.com>
2020-05-13 01:53:25 +0200

    Add clean-webpack-plugin to clean dist every build and update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
@@ -40,6 +41,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
